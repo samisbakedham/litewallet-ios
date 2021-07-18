af383e9afc1622eb8b3dd1966078197ae6379654
kcw-grunt <mrkerrywashington@icloud.com>
2021-07-19 00:13:38 +0100

    WIP: Updated models

--- loafwallet/TransferAmountSelectionViewModel.swift
+++ loafwallet/TransferAmountSelectionViewModel.swift
@@ -9,5 +9,53 @@
 import Foundation
 
 class TransferAmountSelectionViewModel: ObservableObject {
-    init() { }
+      
+    @Published
+    var transferAmountString: String = ""
+    
+    var litewalletBalance: Double =  0.0
+    
+    var litecoinCardBalance: Double = 0.0
+    
+    var transferAmount: Double {
+        return Double(transferAmountString) ?? 0.0
+    }
+    
+    init() {
+        
+    }
+    
+    /// Calculating the new wallet balance: Since this is such a trivial calculation it is made more bulletproof so the amount transferred is not violating the business logic.
+    /// - Parameters:
+    ///   - walletType: Which wallet that will doing the transfer
+    ///   - transferAmount: User chooses which amount
+    /// - Returns: the new wallet balance
+    
+    func newBalance(walletType: WalletType, transferAmount: Double) -> Double {
+        
+        var newBalance: Double = 0.0
+        
+        if walletType == .litecoinCard {
+             
+            let amount = transferAmount > litecoinCardBalance ? litecoinCardBalance : transferAmount
+            
+            newBalance = litecoinCardBalance - amount
+            
+        } else if walletType == .litewallet {
+             
+            let amount = transferAmount > litewalletBalance ? litewalletBalance : transferAmount
+            
+            newBalance = litewalletBalance - amount 
+        }
+        
+        return newBalance
+    }
+    
+    func endingBalance(walletType: WalletType, transferAmount: Double) -> Double {
+        return walletType == .litecoinCard ?
+            (litewalletBalance + transferAmount) :
+            (litecoinCardBalance + transferAmount)
+    }
+    
 }
+
